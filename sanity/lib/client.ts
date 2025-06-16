48809f596f765ee7a9b200f5419f90fcbb1e7214
kaungsike <kaungsike9@gmail.com>
2025-06-16 23:43:36 +0630

    fix : change useCdn to true

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false,
+  useCdn: true,
   perspective: "previewDrafts",
   token: process.env.SANITY_API_READ_TOKEN, // Make sure this env var is set with a token that has Viewer or higher role
   withCredentials: true, // also add this line to enable cookies-based auth if you want
